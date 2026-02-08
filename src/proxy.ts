607ba809767f9d472cb9a50395abd1569a762abf
joshua Oyewole <joshpp2013@gmail.com>
2026-02-08 23:45:17 +0100

    last update

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,7 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
 
-
 const protectedRoutes = ["/admin"];
 export function proxy(request: NextRequest) {
     const { pathname } = request.nextUrl;
